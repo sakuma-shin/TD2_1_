420e34daec61c0c2d42d0f5e568276d27b356ef4
sakuma-shin <k023g0084@g.neec.ac.jp>
2024-10-10 11:07:43 +0900

    wall途中

--- Wall.h
+++ Wall.h
@@ -11,14 +11,20 @@ public:
 
 	void DrawQuad(Corners a, int imageGH, int imageWidth, int imageHeight, unsigned int color, float scroll);
 
+	void Stage1Initialize();
+	void Stage2Initialize();
+	void Stage3Initialize();
+
+
+
 private:
-	int wallNum_;
-	Vector2 pos_[8];
-	float width;
-	float height;
-	Corners corners_[8];
+
+	Vector2 pos_[28];
+	float width_[28];
+	float height_[28];
+	Corners corners_[28];
 
 	int GH = Novice::LoadTexture("white1x1.png");
-	
+
 
 };
